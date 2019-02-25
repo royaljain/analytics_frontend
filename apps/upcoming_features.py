7bc9fff8605233230a5fd805f65104682cd9f375
Royal Jain <royaljain0203@gmail.com>
2019-02-25 11:33:16 +0530

    upcoming features tab

---
+++ apps/upcoming_features.py
@@ -0,0 +1,16 @@
+import dash_html_components as html
+
+
+layout = [
+    html.Div(
+        html.H5('Company Location Data : Identify which companies are present near your stores')
+    ),
+    html.Div(
+
+        html.H5('Dish Recommendation : Identify personalized dish recommendation at each consumer level'),
+    ),
+    html.Div(
+
+        html.H5('Menu Recommendation : Identify which dishes should be added or removed from the menu'),
+    ),
+]
